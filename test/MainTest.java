45474132fb2096df760a4e38e1f07111be622940
kim jihwan <kjhwan0802@naver.com>
2024-05-27 13:28:03 +0900

    add Test code / junit-jupiter 설치 필요

---
+++ test/MainTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @org.junit.jupiter.api.Test
+    void sum1() {
+        Main main = new Main();
+        assertEquals(main.sum(1, 2), 3);
+    }
+
+    @Test
+    void sum2() {
+        Main main = new Main();
+        assertEquals(main.sum(1, 2), 4);
+    }
+}

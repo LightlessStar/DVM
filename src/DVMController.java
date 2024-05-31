8f3d926ca90a45318ec3b5d03abc1e3f2ff2abb0
kim jihwan <kjhwan0802@naver.com>
2024-05-31 12:57:16 +0900

    Add controller

---
+++ src/DVMController.java
@@ -0,0 +1,12 @@
+import org.json.JSONObject;
+
+import java.util.HashMap;
+
+public class DVMController {
+    private String verify_code;
+    private JSONObject stock_msg_JSON;
+    private JSONObject prepayment_msg_JSON;
+    private int[] coord_xy;
+    private HashMap<String, int[]> other_dvm_coord;
+    private HashMap<String, JSONObject>
+}

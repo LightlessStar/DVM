30b991881c1c9ffd0e6e7137a6f44730ebddce9e
kim jihwan <kjhwan0802@naver.com>
2024-06-03 14:44:44 +0900

    DVM Controller & main 수정

--- src/main/java/DVMController.java
+++ src/main/java/DVMController.java
@@ -2,6 +2,8 @@ import java.io.*;
 import java.net.*;
 import java.util.HashMap;
 import org.json.JSONObject;
+//예제 파일 합치기
+import com.google.gson.Gson;
 
 public class DVMController {
     private String verify_code;
@@ -46,8 +48,12 @@ public class DVMController {
 
         return true;
     }
+    public boolean enter_code(String verify_code) {
+        return true;
+    }
     public boolean send_card_num(int card_id){
-        if (bank.certify_pay(card_id)){
+        int charge = 1000000;
+        if (bank.certify_pay(card_id, charge)){
 
         }
         return true;
@@ -55,95 +61,120 @@ public class DVMController {
 
     private final int PORT = 30303;
 
-    public void runServer() {
+//    public void runServer() {
+//        Thread serverThread = new Thread(() -> {
+//        try (ServerSocket serverSocket = new ServerSocket(PORT)) {
+//            System.out.println("start server");
+//
+//            // 클라이언트로부터 연결을 대기하고, 연결되면 소켓을 생성합니다.
+//            Socket clientSocket = serverSocket.accept();
+//            System.out.println("connect client");
+//
+//            // 클라이언트로부터 받은 데이터를 읽어서 화면에 출력합니다.
+//            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+//            String line;
+//            while ((line = in.readLine()) != null) {
+//                System.out.println("client msg: " + line);
+//
+//                // JSON 형식의 문자열을 JSONObject로 변환합니다.
+//                JSONObject receivedJson = new JSONObject(line);
+//
+//                // JSONObject에서 원하는 데이터를 추출합니다.
+//                String name = receivedJson.getString("name");
+//                int age = receivedJson.getInt("age");
+//
+//                System.out.println("name: " + name);
+//                System.out.println("age: " + age);
+//            }
+//
+//            // 클라이언트와의 연결을 종료합니다.
+//            clientSocket.close();
+//            System.out.println("client connect finish");
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        });
+//        serverThread.start();
+//    }
+//
+//    public void runClient() {
+//        Thread clientThread = new Thread(() -> {
+//            try (Socket socket = new Socket("localhost", PORT)) {
+//                System.out.println("connected server");
+//
+//                // 서버로 보낼 JSON 데이터를 생성합니다.
+//                JSONObject jsonToSend = new JSONObject();
+//                jsonToSend.put("name", "John Doe");
+//                jsonToSend.put("age", 30);
+//
+//                // 서버로 데이터를 전송합니다.
+//                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+//                out.println(jsonToSend.toString());
+//
+//                // 서버에서 받은 응답을 읽어서 화면에 출력합니다.
+//                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+//                String response = in.readLine();
+//                System.out.println("response from server: " + response);
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//        clientThread.start();
+//    }
+
+
+
+//    public class JsonSocketServiceImpl {
+//        private Socket socket;
+//
+//
+//        public JsonSocketServiceImpl(Socket socket) {
+//            this.socket = socket;
+//        }
+//
+//        public void start() {
+//            try {
+//                this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+//                this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+//            } catch (Exception e) {
+//                throw new RuntimeException("Error initializing streams", e);
+//            }
+//        }
+//
+//        public void stop() {
+//            try {
+//                writer.close();
+//                reader.close();
+//                socket.close();
+//            } catch (Exception e) {
+//                throw new RuntimeException("Error closing streams", e);
+//            }
+//        }
+//
+//
+//        public void sendMessage(Object message) {
+//            writer.println(gson.toJson(message));
+//        }
+//
+//
+//        public <T> T receiveMessage(Class<T> clazz) {
+//            try {
+//                return gson.fromJson(reader.readLine(), clazz);
+//            } catch (Exception e) {
+//                throw new RuntimeException("Error receiving message", e);
+//            }
+//        }
+//    }
+
+
+    private PrintWriter writer;
+    private BufferedReader reader;
+    private final Gson gson = new Gson();
+
+    public JSONObject res_stock_msg(){
         Thread serverThread = new Thread(() -> {
         try (ServerSocket serverSocket = new ServerSocket(PORT)) {
-            System.out.println("start server");
-
-            // 클라이언트로부터 연결을 대기하고, 연결되면 소켓을 생성합니다.
-            Socket clientSocket = serverSocket.accept();
-            System.out.println("connect client");
-
-            // 클라이언트로부터 받은 데이터를 읽어서 화면에 출력합니다.
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            String line;
-            while ((line = in.readLine()) != null) {
-                System.out.println("client msg: " + line);
-
-                // JSON 형식의 문자열을 JSONObject로 변환합니다.
-                JSONObject receivedJson = new JSONObject(line);
-
-                // JSONObject에서 원하는 데이터를 추출합니다.
-                String name = receivedJson.getString("name");
-                int age = receivedJson.getInt("age");
-
-                System.out.println("name: " + name);
-                System.out.println("age: " + age);
-            }
-
-            // 클라이언트와의 연결을 종료합니다.
-            clientSocket.close();
-            System.out.println("client connect finish");
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        });
-        serverThread.start();
-    }
-
-    public void runClient() {
-        Thread clientThread = new Thread(() -> {
-            try (Socket socket = new Socket("localhost", PORT)) {
-                System.out.println("connected server");
-
-                // 서버로 보낼 JSON 데이터를 생성합니다.
-                JSONObject jsonToSend = new JSONObject();
-                jsonToSend.put("name", "John Doe");
-                jsonToSend.put("age", 30);
-
-                // 서버로 데이터를 전송합니다.
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(jsonToSend.toString());
-
-                // 서버에서 받은 응답을 읽어서 화면에 출력합니다.
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String response = in.readLine();
-                System.out.println("response from server: " + response);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        clientThread.start();
-    }
-
-    public void runClient2() {
-        Thread clientThread = new Thread(() -> {
-            try (Socket socket = new Socket("localhost", PORT)) {
-                System.out.println("connected server");
-
-                // 서버로 보낼 JSON 데이터를 생성합니다.
-                JSONObject jsonToSend = new JSONObject();
-                jsonToSend.put("name", "John Doe");
-                jsonToSend.put("age", 30);
-
-                // 서버로 데이터를 전송합니다.
-                PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
-                out.println(jsonToSend.toString());
-
-                // 서버에서 받은 응답을 읽어서 화면에 출력합니다.
-                BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                String response = in.readLine();
-                System.out.println("response from server: " + response);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        clientThread.start();
-    }
-
-    private JSONObject res_stock_msg(JSONObject msg){
-        try (ServerSocket serverSocket = new ServerSocket(port)) {
-            System.out.println("Server is listening on port " + port);
+            System.out.println("Server is listening on PORT " + PORT);
 
             while (true) {
                 Socket clientSocket = serverSocket.accept();
@@ -154,43 +185,82 @@ public class DVMController {
                     throw new RuntimeException("Error initializing streams", e);
                 }
                 // 여기서 클라이언트로부터 메시지를 받고, 응답을 보낼 수 있습니다.
-                service.sendMessage("Hello from server");
 
-                try {
-                    writer.close();
-                    reader.close();
-                    clientSocket.close();
-                } catch (Exception e) {
-                    throw new RuntimeException("Error closing streams", e);
-                }
+                String other_dvm_msg = reader.readLine();
+                System.out.println("i'm server : receive msg " + other_dvm_msg);
+
+                JSONObject res_msg = new JSONObject();
+                JSONObject item_JSON = new JSONObject();
+                res_msg.put("msg_type","req_stock");
+                res_msg.put("src_id","Team6");
+                res_msg.put("dst_id","0");
+                item_JSON.put("item_code",20);
+                item_JSON.put("count",10);
+                item_JSON.put("coor_x", coord_xy[0]);
+                item_JSON.put("coor_y", coord_xy[1]);
+                res_msg.put("msg_content", item_JSON);
+                writer.println(res_msg);
+
+
+//                try {
+//                    writer.close();
+//                    reader.close();
+//                    clientSocket.close();
+//                } catch (Exception e) {
+//                    throw new RuntimeException("Error closing streams", e);
+//                }
             }
         } catch (Exception e) {
             System.out.println("Server exception: " + e.getMessage());
             e.printStackTrace();
         }
-        return msg;
+        });
+        serverThread.start();
+        return null;
     }
     private JSONObject req_stock_msg(JSONObject msg){
-        try (Socket socket = new Socket(host, port)) {
-            JsonSocketServiceImpl service = new JsonSocketServiceImpl(socket);
-            service.start();
+        Thread clientThread = new Thread(() -> {
+
+        try (Socket socket = new Socket("localhost", PORT)) {
+            try {
+                this.writer = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "UTF-8"), true);
+                this.reader = new BufferedReader(new InputStreamReader(socket.getInputStream(), "UTF-8"));
+            } catch (Exception e) {
+                throw new RuntimeException("Error initializing streams", e);
+            }
 
             // 서버로 메시지를 보내고 응답을 받습니다.
-            service.sendMessage(msg);
-            Message response = service.receiveMessage(Message.class);
-            JSONObject msg_content = new JSONObject();
-            msg_content = response.getJSONObject("msg_content");
-            other_dvm_stock.put(response.get("src_id"),msg_content);
-            int[] coor = new int[2];
-            coor[0] =  msg_content.getInt("coor_x");
-            coor[1] = msg_content.getInt("coor_y");
-            other_dvm_coord.put(response.get("src_id"), coor);
+            writer.println(msg);
+            System.out.println("i'm client : send to server" + msg);
 
-            service.stop();
+            String response_other_dvm = reader.readLine();
+            System.out.println("i'm client : server res receive!!" + response_other_dvm);
+
+
+            //Message response = service.receiveMessage(Message.class);
+            JSONObject msg_content = new JSONObject(response_other_dvm);
+            System.out.println(msg_content);
+            //msg_content = response.getJSONObject("msg_content");
+            other_dvm_stock.put(msg_content.get("src_id").toString(),msg_content);
+            int[] coor = new int[2];
+            coor[0] =  msg_content.getJSONObject("msg_content").getInt("coor_x");
+            coor[1] = msg_content.getJSONObject("msg_content").getInt("coor_y");
+            other_dvm_coord.put(msg_content.get("src_id").toString(), coor);
+
+            try {
+                writer.close();
+                reader.close();
+                socket.close();
+            } catch (Exception e) {
+                throw new RuntimeException("Error closing streams", e);
+            }
+//            return msg_content;
         } catch (Exception e) {
             System.out.println("Client exception: " + e.getMessage());
             e.printStackTrace();
         }
-        return res;
+        });
+        clientThread.start();
+        return null;
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,12 +2,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("team6 DVM start");
 
+        //DVM UI main test
+        //DVMUI dvmui = new DVMUI();
+
+
+        //DVM Controller main test
         DVMController dvmController = new DVMController();
 //        dvmController.runServer();
 //        dvmController.runClient();
 //        dvmController.runClient2();
-
-        dvmController.request_stock_msg(3,4);
+        dvmController.res_stock_msg();
+        dvmController.request_stock_msg(3, 4);
+//        dvmController.request_stock_msg(3, 5);
+//        dvmController.request_stock_msg(3, 6);
+//        dvmController.request_stock_msg(3, 7);
 
     }
 }

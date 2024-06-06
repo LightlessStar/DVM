38b3b361f56444d087be4e3ea23a3cd694dae49d
Kim jihwan <kjhwan0802@naver.com>
2024-06-06 21:05:18 +0900

    DVMStock check stock 수정 -> DCD 반영 완료

--- src/main/java/DVMStock.java
+++ src/main/java/DVMStock.java
@@ -3,32 +3,33 @@ public class DVMStock {
     int[] item;
     boolean[] Is_our_item;
     DVMController dvmController;
+
     public DVMStock() {
         item = new int[20];
-        dvmController= new DVMController();
-        Is_our_item=new boolean[20];
-        for(int i=0;i<item.length;i++){
-            if(i<7){
-                item[i]=i;
-                Is_our_item[i]=true;
-            }else{
-                item[i]=0;
-                Is_our_item[i]=false;
-            }
+        //item 개수 99개로 초기화
+        for (int i = 0; i < item.length; i++) {
+            item[i] = 99;
         }
+
+        Is_our_item = new boolean[20];
+        //우리가 관리하는 Item 정하기
+        Is_our_item[0] = true;
+        Is_our_item[1] = true;
+        Is_our_item[2] = true;
+        Is_our_item[3] = true;
+        Is_our_item[4] = true;
+        Is_our_item[5] = true;
+        Is_our_item[6] = true;
     }
 
-    public int check_stock(int item_code, int count){
-        if(!Is_our_item[item_code-1] || item[item_code-1]<count){
-            if(dvmController.request_stock_msg(item_code, count)){
-                return 1;
-            }else{
-                return 2;
-            }
-        }else if(count>20 || count<0) {
-            return 3;
-        }else {
-            item[item_code-1]-=count;
+    public int check_stock(int item_code, int count) {
+        if (!Is_our_item[item_code - 1] || item[item_code - 1] < count) {
+            return 1;
+        } else if (count > 20 || count < 0) {
+            System.out.println("EXP: 구매 가능 개수 20개를 초과했거나 count가 올바르지 않습니다");
+            return -1;
+        } else {
+            item[item_code - 1] -= count;
             return 0;
         }
     }
@@ -37,17 +38,17 @@ public class DVMStock {
         return item;
     }
 
-  public boolean add_item(int item_code, int stock) {
-    if (Is_our_item[item_code - 1]) {
-      int temp = item[item_code - 1];
-      item[item_code - 1] += stock;
-      if (item[item_code - 1] - temp == stock) {
-        return true;
-      } else {
-        return false;
-      }
-    } else {
-      return false;
+    public boolean add_item(int item_code, int stock) {
+        if (Is_our_item[item_code - 1]) {
+            int temp = item[item_code - 1];
+            item[item_code - 1] += stock;
+            if (item[item_code - 1] - temp == stock) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
     }
-  }
 }

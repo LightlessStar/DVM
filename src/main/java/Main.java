69e3c8ebb9010b9cdfa805d687dfb2d07c32802f
Kim jihwan <kjhwan0802@naver.com>
2024-06-07 11:53:13 +0900

    stock 공유 main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,20 +3,23 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("team6 DVM start");
 
+        //공통 STock 인스턴스
+        DVMStock dvmStock = new DVMStock();
+
         //DVM UI main test
         //DVMUI dvmui = new DVMUI();
 
         //DVM Controller main test
-        DVMController server_dvmController = new DVMController();
-        DVMController cli_dvmController = new DVMController();
-        DVMController cli2_dvmController = new DVMController();
+        DVMController server_dvmController = new DVMController(dvmStock);
+        DVMController cli_dvmController = new DVMController(dvmStock);
+        DVMController cli2_dvmController = new DVMController(dvmStock);
 
-        server_dvmController.res_stock_msg();
-        cli_dvmController.request_stock_msg(20, 4);
-        cli2_dvmController.prepay_info(3, 5);
+//        server_dvmController.res_stock_msg();
+//        cli_dvmController.request_stock_msg(20, 4);
+//        cli2_dvmController.prepay_info(3, 5);
 ////        cli_dvmController.request_stock_msg(3, 6);
 ////        cli_dvmController.request_stock_msg(3, 7);
 
-        DVMUI dvmui = new DVMUI();
+        DVMUI dvmui = new DVMUI(dvmStock);
     }
 }
